732ecdeb675c530b272311ea3aa31720007ae734
Mekontso Anderson Sa'a <mekontsoanderson@gmail.com>
2021-11-03 09:08:10 +0100

    Part IV - Container Orchestration with Kubernetes

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aae277e22314d4781b68128efc635791-871343390.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
